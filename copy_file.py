cea9362fd883dc6ff1414a7b8622f0130ab4792a
alessio rea <alexei.80@hotmail.fr>
2021-03-22 10:44:56 +0100

    progress

---
+++ copy_file.py
@@ -0,0 +1,17 @@
+import os
+
+args = {
+    'KeyPair' : "/Users/alexei/docs/mykeypairP4.pem",
+    'source_file' : "/Users/alexei/docs/mykeypairP4.pem",
+    'dns' : "ec2-user@ec2-18-220-88-190.us-east-2.compute.amazonaws.com",
+    'local_path' : "/home/hadoop"
+}
+
+
+command = '''scp -i {KeyPair} {source_file} {dns}:{local_path}'''.format(**args)
+
+
+#print(command)
+
+os.system(command)
+
--- create_cluster.py
+++ create_cluster.py
@@ -18,15 +18,5 @@ aws emr create-cluster --name {name} --use-default-roles --release-label emr-5.2
 
 #print(command)
 
-os.system(command)
-
-
-connect = "ssh -i "mykeypairP4.pem" root@ec2-18-220-88-190.us-east-2.compute.amazonaws.com"
-
-
-ssh -i /Users/alexei/docs/mykeypairP4.pem -N -D 8157 root@ec2-18-220-88-190.us-east-2.compute.amazonaws.com
-
-
-
-scp -i /Users/alexei/docs/mykeypairP4.pem /Users/alexei/docs/mykeypairP4.pem ec2-user@ec2-18-220-88-190.us-east-2.compute.amazonaws.com:/home/hadoop
+#os.system(command)
 

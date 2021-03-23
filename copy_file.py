856215d1fbf7c21d7099db2309e6b3941976b068
alessio rea <alexei.80@hotmail.fr>
2021-03-23 09:41:40 +0100

    progress

--- copy_file.py
+++ copy_file.py
@@ -2,13 +2,14 @@ import os
 
 args = {
     'KeyPair' : "/Users/alexei/docs/mykeypairP4.pem",
-    'source_file' : "/Users/alexei/docs/mykeypairP4.pem",
-    'dns' : "ec2-user@ec2-18-220-88-190.us-east-2.compute.amazonaws.com",
-    'local_path' : "/home/ec2-user"
+    'source_file1' : "/Users/alexei/sparkify_EMR/etl.py",
+    'source_file2' : "/Users/alexei/docs/dl.cfg",
+    'dns' : "hadoop@ec2-3-16-155-88.us-east-2.compute.amazonaws.com",
+    'local_path' : "/home/hadoop"
 }
 
 
-command = '''scp -i {KeyPair} {source_file} {dns}:{local_path}'''.format(**args)
+command = '''scp -i {KeyPair} {source_file1} {dns}:{local_path}'''.format(**args)
 
 print('*'*20)
 print(command)
@@ -16,3 +17,11 @@ print('*'*20)
 
 os.system(command)
 
+
+command = '''scp -i {KeyPair} {source_file2} {dns}:{local_path}'''.format(**args)
+
+print('*'*20)
+print(command)
+print('*'*20)
+
+os.system(command)
--- proxy_setup.py
+++ proxy_setup.py
@@ -2,7 +2,7 @@ import os
 
 args = {
     'KeyPair' : "/Users/alexei/docs/mykeypairP4.pem",
-    'dns' : "ec2-user@ec2-3-139-59-143.us-east-2.compute.amazonaws.com",
+    'dns' : "hadoop@ec2-3-16-155-88.us-east-2.compute.amazonaws.com",
     'port' : 8157
 }
 
